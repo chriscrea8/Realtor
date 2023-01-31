f839248eb57eaae2ab2eeb7688296e48c840213b
Christian Chukwu <52824325+chriscrea8@users.noreply.github.com>
2023-01-31 07:49:04 +0100

    setup api data

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const baseUrl = 'https://bayut.p.rapidapi.com'
+export const baseUrl = 'https://bayut.p.rapidapi.com'
 
 export const fetchApi = async (url) => {
-    const response = await axios.get((url), {
+    const {data} = await axios.get((url), {
         headers: {
             'X-RapidAPI-Key': 'fc618951fbmsh9215c8eea44b624p1320ccjsn616711cd18b3',
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
